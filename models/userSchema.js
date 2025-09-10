e3f8696abe9a7783d1f653444a33da9211170092
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-09-10 14:07:40 +0200

    create user model

---
+++ models/userSchema.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "L'adresse e-mail doit être valide ex: exemple@domaine.com .",
+      ],
+    },
+    password :{
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "L'adresse e-mail doit être valide ex: exemple@domaine.com .",
+      ],
+    },
+    
+    role: {
+      type: String,
+      enum: ["admin", "client"],
+    },
+
+    user_image: {
+      data: { type: Buffer, required: false },
+      contentType: { type: String, required: false }
+    },
+  },
+
+  { timestamps: true }
+);
+
+userSchema.post("save", async function (res, req, next) {
+  console.log("user add -------------------------------");
+});
+
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt();
+    const user = this;
+    user.password = await bcrypt.hash(user.password, salt);
+    user.count = user.count + 1;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
