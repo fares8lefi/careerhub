8fde01ef1541f2c7d64001ddee6a5a1defd694a8
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-09-11 11:29:57 +0200

    create login Router

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const userController= require("../controller/userController")
 /* GET users listing. */
 router.post("/addUser",userController.addUser)
-
-
+router.get("/getUsers",userController.getUsers)
+router.get("/deleteUser/:id",userController.deleteUser)
+router.get("/loginUser",userController.loginUser)
 module.exports = router;
